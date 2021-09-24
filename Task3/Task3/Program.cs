f5d5a71738d02c2365427c03eabeb1446fe331d7
hutman2083 <mr.2003sasha@mail.ru>
2021-09-24 11:33:20 +0300

    kish

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число элементов в массиве\n");
+            int[] nums = new int[5];
+            int repeatvalue = 0;
+            int index = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int schitatel = 1;
+
+                for (int j = i+1; j < nums.Length; j++)
+                {
+
+                    if (nums[i] == nums[j] )
+                    {
+                        schitatel++;
+                    }
+
+                }
+
+                if (schitatel <= repeatvalue)
+                {
+                    continue;
+                }
+                repeatvalue=schitatel;
+                index = i;
+            }
+            Console.WriteLine($"Число {nums[index]} встречается чаще всего {repeatvalue} раз(а)");
+        }
+    }
+}
